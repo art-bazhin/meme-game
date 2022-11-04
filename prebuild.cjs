eb76c98f1e86ab00c9ef374d3bdca5696f1e8696
art-bazhin <bazhin.artyom@yandex.ru>
2022-11-05 01:13:11 +0600

    fix prebuild script

--- prebuild.cjs
+++ prebuild.cjs
@@ -1,9 +1,9 @@
 const { rmSync } = require('fs');
-const { cpSync } = require('fs-extra');
+const { copy } = require('fs-extra');
 
 const handler = (e) => {
   if (e) console.error(e);
 };
 
 rmSync('./dist', { recursive: true, force: true }, handler);
-cpSync('./src/assets/', './dist/static/', { recursive: true }, handler);
+copy('./src/assets/', './dist/static/', { recursive: true }, handler);
